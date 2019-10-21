e6c7e9e44970a0beea60e215a1a904f35817f0ad
Joaquin Casal <joaquinarielcasal@gmail.com>
2019-10-21 20:33:18 -0300

    Add Store model

---
+++ models/Store.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs')
+
+const storeSchema = new mongoose.Schema({
+  name: {type: String, trim: true, required: 'Enter a store name'},
+  slug: String,
+  description: {type: String, trim: true},
+  tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO: slugs must be unique
+});
+
+module.exports = mongoose.model('Store', storeSchema);
