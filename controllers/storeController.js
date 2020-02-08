c5efc9e1e4c94b95c14980016eb3e55f609d8392
Joaquin Casal <joaquinarielcasal@gmail.com>
2020-02-08 14:03:11 -0300

    Edit stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,11 +11,26 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`)
+  req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores })
+  res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true, runValidators: true }
+  ).exec();
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`)
+  res.redirect(`/stores/${store._id}/edit`);
 }
