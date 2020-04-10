eac709de560816345f78401af70145bdc628cfe3
Joaquin Casal <joaquinarielcasal@gmail.com>
2020-04-10 12:50:15 -0300

    Edit account

--- routes/index.js
+++ routes/index.js
@@ -37,4 +37,7 @@ router.post('/register', userController.validateRegister,
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
