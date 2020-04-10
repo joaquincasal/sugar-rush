0b86773bac4c33634bf05893e45eddd837ff23e8
Joaquin Casal <joaquinarielcasal@gmail.com>
2020-04-10 12:16:17 -0300

    Add users
    - Register and login users
    - Protected routes

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String, unique: true, lowercase: true, trim: true, validate: [validator.isEmail, 'Invalid email address'], required: 'Enter an email address'
+  },
+  name: { type: String, required: 'Enter a name', trim: true }
+});
+
+userSchema.virtual('gravatar').get(function () {
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`;
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
